94adefcb404941db875e050cbbcf194b4d0a87a8
johnpauljpc <jpcwork081@gmail.com>
2023-03-06 13:33:58 +0100

    SMTP server congiguration

--- website/settings.py
+++ website/settings.py
@@ -133,3 +133,8 @@ LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
 
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "jpcdjango@gmail.com"
+EMAIL_HOST_PASSWORD = "srcgjwixxkpnilsa"
