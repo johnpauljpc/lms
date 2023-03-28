0d9e9a32bce17741c838d3d519d78dc72a21b960
johnpauljpc <jpcwork081@gmail.com>
2023-03-28 02:27:01 +0100

    tweaked

--- lms/admin.py
+++ lms/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import (Categories, Author, Course, Level,
                      things_you_wil_learn, Course_Requirements,
-                     Lesson, Video)
+                     Lesson, Video, Language)
 
 class things2learnInline(admin.TabularInline):
     model = things_you_wil_learn
@@ -27,3 +27,4 @@ admin.site.register(things_you_wil_learn)
 admin.site.register(Course_Requirements)
 admin.site.register(Lesson)
 admin.site.register(Video)
+admin.site.register(Language)
